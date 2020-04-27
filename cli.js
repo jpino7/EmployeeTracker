df20189f9fb4c1f80f6de5d8bd65897b13d7f7cb
JRPino <pino3789@gmail.com>
2020-04-27 15:58:34 -0700

    Added startPrompting Function to kick off cli.js

--- cli.js
+++ cli.js
@@ -5,14 +5,34 @@ const consoleTable = require("console.table");
 
 // Connection Set up
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "rootroot",
-  database: "employee_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "employee_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as ID " + connection.threadId);
+
+    startPrompting();
 });
+
+function startPrompting() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Add Department",
+            "Add Role",
+            "Add Employee",
+            "View Department",
+            "View Role",
+            "View Employee",
+            "Update Employee Role",
+            "EXIT"]
+    }).then(function (answer) {
+        console.log("You selected: " + answer.action);
+    });
+}
